d9f9064139b7fd02ea2ef71614af2a0e9d3f48fb
Carlo Franzelli <cfranzelli@sorint.it>
2021-07-16 10:40:20 +0200

    bundling start

---
+++ 04.bundling/webapp/app.js
@@ -0,0 +1,27 @@
+const { send, once } = __COMMUNICATION_WITH_MAIN_PROCESS;
+
+window.addEventListener("DOMContentLoaded", () => {
+  const { chrome, node, electron } = __VARIABLES_FROM_MAIN_PROCESS;
+  document.getElementById("node-version").innerText = node;
+  document.getElementById("chrome-version").innerText = chrome;
+  document.getElementById("electron-version").innerText = electron;
+
+
+  retrieveData();
+});
+
+const retrieveData = () => {
+  const response = `response-${Math.ceil(Math.random() * 1000)}`;
+  once(response, (_, response) => {
+    console.log(response);
+  });
+  send("retrieve-data", { payload: {}, response });
+}
+
+document.getElementById("my-button").addEventListener("click", () => {
+  const response = `response-${Math.ceil(Math.random() * 1000)}`;
+  once(response, (_, response) => {
+    console.log(response);
+  });
+  send("store-data", { payload: { foo: 'bar' }, response });  
+});
